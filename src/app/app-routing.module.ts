f37e54a8e9df6fd82f201cd809fd37ba84230414
Umang Gohel <gohelumang12@gmail.com>
2023-05-22 10:42:26 +0530

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { AboutComponent } from './MyComponents/about/about.component';
+import { ContactComponent } from './MyComponents/contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { TodosComponent } from './MyComponents/todos/todos.component';
 import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 import { FormsModule } from '@angular/forms';
+import { ContactComponent } from './MyComponents/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
